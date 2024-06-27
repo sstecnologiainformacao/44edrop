10ee9cc85db5fa916d9b06f1e477b2a7d12b5770
João Lucas <sstecnologiainformacao@gmail.com>
2024-06-27 16:31:42 -0300

    adiciona o banner

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Banner from "../components/Banner";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
-
-      <h1>
-        <Header />
-      </h1>
+      <Header />
+      <main className='max-w-screen-2xl mx-auto'>
+        <Banner />
+      </main>
     </div>
   );
 }
